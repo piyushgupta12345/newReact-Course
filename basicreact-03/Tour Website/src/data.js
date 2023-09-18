0325f8309acde63d9ae9a1a88adb14ae383a21ca
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-09-18 12:52:54 +0530

    basic-project

---
+++ basicreact-03/Tour Website/src/data.js
@@ -0,0 +1,51 @@
+const data = [
+    {
+        id: "1",
+        name: "Agra",
+        info: "Agra's notable historical period began during Sikandar Lodi's reign, but the golden age of the city began with the Mughals. Agra was the foremost city of the Indian subcontinent and the capital of the Mughal Empire under Mughal emperors Babur, Humayun, Akbar, Jahangir and Shah Jahan. Under Mughal rule, Agra became a centre for learning, arts, commerce, and religion, and saw the construction of the Agra Fort, Sikandra and Agra's most prized monument, the Taj Mahal, built by Shah Jahan as a mausoleum for his favourite empress. With the decline of the Mughal empire in the late 18th century, the city fell successively first to Marathas and later to the East India Company. After Independence, Agra has developed into an industrial town, with a booming tourism industry, along with footwear, leather and other manufacturing. The Taj Mahal and the Agra Fort are UNESCO World Heritage Sites. The city features mild winters, hot and dry summers and a monsoon season, and is famous for its Mughlai cuisine. Agra is included on the Golden Triangle tourist circuit, along with Delhi and Jaipur; and the Uttar Pradesh Heritage Arc, a tourist circuit of Uttar Pradesh, along with Lucknow and Varanasi",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Taj_Mahal%2C_Agra%2C_India.jpg/1280px-Taj_Mahal%2C_Agra%2C_India.jpg",
+        price: "35999"
+    },
+    {
+        id: "2",
+        name: "Jaipur",
+        info: "Jaipur is the capital of India’s Rajasthan state. It evokes the royal family that once ruled the region and that, in 1727, founded what is now called the Old City, or “Pink City” for its trademark building color. At the center of its stately street grid (notable in India) stands the opulent, colonnaded City Palace complex. With gardens, courtyards and museums, part of it is still a royal residence",
+        image: "https://lh3.googleusercontent.com/p/AF1QipPvRLMbvf0so_nFsawhu_CnVceeNcM3RL1Eu-lH=s1360-w1360-h1020",
+        price: "79099"
+    },
+    {
+        id: "3",
+        name: "Goa",
+        info: "Goa is a state in western India with coastlines stretching along the Arabian Sea. Its long history as a Portuguese colony prior to 1961 is evident in its preserved 17th-century churches and the area’s tropical spice plantations. Goa is also known for its beaches, ranging from popular stretches at Baga and Palolem to those in laid-back fishing villages such as Agonda",
+        image: "https://www.tourmyindia.com/states/goa/image/our-lady-of-immaculate-conception-church-goa.webp",
+        price: "99999"
+    },
+    {
+        id: "4",
+        name: "Vanarasi",
+        info: "Varanasi is a city in the northern Indian state of Uttar Pradesh dating to the 11th century B.C. Regarded as the spiritual capital of India, the city draws Hindu pilgrims who bathe in the Ganges River’s sacred waters and perform funeral rites. Along the city's winding streets are some 2,000 temples, including Kashi Vishwanath, the “Golden Temple,” dedicated to the Hindu god Shiva.",
+        image: "https://lh3.googleusercontent.com/p/AF1QipMh1HRJY3z9wMQU49uMU6ipbfkXs1LovhQPDNAh=s1360-w1360-h1020",
+        price: "49000"
+    },
+    {
+        id: "5",
+        name: "Kochi",
+        info: "Kochi (also known as Cochin) is a city in southwest India's coastal Kerala state. It has been a port since 1341, when a flood carved out its harbor and opened it to Arab, Chinese and European merchants. Sites reflecting those influences include Fort Kochi, a settlement with tiled colonial bungalows and diverse houses of worship. Cantilevered Chinese fishing nets, typical of Kochi, have been in use for centuries",
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/36/7d/9a/cherai-beach.jpg?w=700&h=500&s=1",
+        price: "71999"
+    },
+    {
+        id: "6",
+        name: "Lucknow",
+        info: "Lucknow, a large city in northern India, is the capital of the state of Uttar Pradesh. Toward its center is Rumi Darwaza, a Mughal gateway. Nearby, the 18th-century Bara Imambara shrine has a huge arched hall. Upstairs, Bhool Bhulaiya is a maze of narrow tunnels with city views from its upper balconies. Close by, the grand Victorian Husainabad Clock Tower was built as a victory column in 1881",
+        image: "https://lh3.googleusercontent.com/proxy/zJ58Fk3NYovlloRrX2CDJNbTs2pkROt4YB_ce4cs1fxduHUTyWgQgzWLjTXehTFpiVkcXTHX8ZsnRL7IYDpDl53_lLqPjGly8IdOXAOj9G8UTcf1I1qU4_JeMnEhl8Ztf7_8qpwWjcWTYvjfZrU7XOmGjzVaJA=s1360-w1360-h1020",
+        price: "19999"
+    },
+    {
+        id: "7",
+        name: "Darjeeling",
+        info: "Darjeeling is a town in India's West Bengal state, in the Himalayan foothills. Once a summer resort for the British Raj elite, it remains the terminus of the narrow-gauge Darjeeling Himalayan Railway, or “Toy Train,” completed in 1881. It's famed for the distinctive black tea grown on plantations that dot its surrounding slopes. Its backdrop is Mt. Kanchenjunga, among the world’s highest peaks.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Tea_Estate%2C_Darjeeling.jpg/1280px-Tea_Estate%2C_Darjeeling.jpg",
+        price: "77999"
+    },
+]
